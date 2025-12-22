0d8d8bd54a0a860347bcad76f2e6084309ea002c
spiredBongo <bogdanbosoanca04@gmail.com>
2025-12-22 16:15:20 +0200

    1.

---
+++ src/main/java/org/example/cacheproxy/CacheProxyApplication.java
@@ -0,0 +1,13 @@
+package org.example.cacheproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cacheproxy/CacheProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cacheproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
